92b3d89a182e879defa0ced9f60eef14134fb415
Ahmad-Alanati <ahmadjehadalanati1@gmail.com>
2023-07-18 08:28:27 +0300

    alomst done with the app

---
+++ snacks/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import SnackListView,SnackDetailView,SnackCreateView,SnackUpdateView #,SnackDeleteView
+
+urlpatterns = [
+    path("",SnackListView.as_view(), name = 'snack_list'),
+    path("<int:pk>",SnackDetailView.as_view(), name = "snack_detail"),
+    path('create/',SnackCreateView.as_view(), name = "create_snack"),
+    path('<int:pk>/update/',SnackUpdateView.as_view(), name = "update_snack")
+]
--- snacks/views.py
+++ snacks/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views.generic import ListView,DetailView,CreateView,UpdateView
+from .models import Snack
 
 # Create your views here.
+class SnackListView(ListView):
+    template_name = "snack_list.html"
+    model = Snack
+
+class SnackDetailView(DetailView):
+    template_name = "snack_detail.html"
+    model = Snack
+
+class SnackCreateView(CreateView):
+    template_name = "create_snack.html"
+    model = Snack
+    fields = ['title',"purchaser","description"]
+
+class SnackUpdateView(UpdateView):
+    template_name = "update_snack.html"
+    model = Snack
+    fields = ['title',"purchaser","description"]
+    
