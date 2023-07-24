470df76695bba0a42719287dd06c4be3de798950
Ahmad-Alanati <ahmadjehadalanati1@gmail.com>
2023-07-24 14:11:43 +0300

    done with the crud app

--- snacks/views.py
+++ snacks/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView,DetailView,CreateView,UpdateView
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
 from .models import Snack
+from django.urls import reverse,reverse_lazy
 
 # Create your views here.
 class SnackListView(ListView):
@@ -20,4 +21,10 @@ class SnackUpdateView(UpdateView):
     template_name = "update_snack.html"
     model = Snack
     fields = ['title',"purchaser","description"]
+    success_url = reverse_lazy('snack_list')
+    
+class SnackDeleteView(DeleteView):
+    template_name = "delete_snack.html"
+    model = Snack
+    success_url = reverse_lazy('snack_list')
     
